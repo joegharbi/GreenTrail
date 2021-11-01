1845938e14f3568513e149640f90b742cbd1abd3
joegh <joe.gharbi@gmail.com>
2021-11-01 18:30:34 +0100

    Refactoring the master and home page and navbar plus adding the logo for our website.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+      <!-- CSS only -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <!-- JavaScript Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Green Trail')</title>
-    <link rel="stylesheet" href=" {{ asset('css/master.css') }} ">  
-    {{-- assest starts from public/... --}}
+  <title>
+      @yield('title')
+  </title>
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href=" {{ asset('css/master.css') }} ">
   </head>
   <body>
-    @include('layouts.navbar')
+    @extends('layouts.navbar')
+    <div class=container")>
     @yield('content')
-    @include('layouts.sidebar')
-   
-    {{-- @section('sidebar')
-       <p> This sidebar shoud included at all page of code(layout.master) </p>
-    @show --}}
+    </div>
   </body>
 </html>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,30 @@
-<ul class="navbar">
-    <li><a href="{{route("home")}}">Home</a></li>
-    <li><a href="{{ route("about-us") }}"> About</a></li>
-    <li><a href="{{ route("contact") }}"> Contact</a></li>
-    <li><a href="/category"> Category</a></li>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            <img src="public/logo.png" alt="" width="30" height="24">
+            </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="{{route("about-us")}}">About</a>
+                <a class="nav-link" href="{{route("contact")}}">Contact</a>
+            </div>
+        </div>
+        @if (Route::has('login'))
+                @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 ">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 ">Log in</a>
 
-  </ul>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 ">Register</a>
+                    @endif
+                @endauth
+        @endif
+    </div>
+</nav>
+
+
