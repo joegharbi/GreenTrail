e3323c9f0ee38e0741b29fab2010c42d14b85c8b
mhmdkadiry <m.kadiry@aplimedia.net>
2021-11-17 17:47:29 +0100

    Merge branch 'dev' of https://github.com/joegharbi/GreenTrail into dev

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,46 @@
         </h2>
     </x-slot>
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+    <!-- write code here -->
+    @section('title', 'Dashboard')
+
+    <!-- css -->
+    <style>
+        .frst_row{
+            min-height: 500px;
+        }
+    </style>
+    <!-- JS -->
+    <script src="{{asset('js/jQuery/jQuery-2.1.4.min.js')}}"></script>
+
+    <div class="container">
+        <div class="row frst_row">
+
+            <!-- icluding CO2 counter -->
+            <div class="col-md-5">
+            @include('pages.history.carbon_counter')
             </div>
+            
+            <!-- including the hisotry table -->
+            <div class="col-md-7">
+            @include('pages.history.history')	
+            </div>
+
         </div>
-    </div> --}}
+        
+        <div class="row">  
+            <!-- including the clendar-->
+            <div class="col-md-5">
+            @include('pages.calendar.calendar')
+            </div>
+
+            <!-- including the calendar table -->
+            <div class="col-md-7">
+            @include('pages.calendar.events')	
+            </div>
+
+        </div>
+    </div>
+    
 </x-app-layout>
 
